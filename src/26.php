af02f96ab0705d918e00838882938fc8a8106ed1
ihsanmarzouk23 <ihsanmarzouk23@gmail.com>
2025-04-05 10:56:14 +0000

    Add 26.php

---
+++ src/26.php
@@ -0,0 +1,8 @@
+<?php
+// Example PHP code for generating random numbers and performing operations
+
+// Generate a random integer between 1 and 100 (inclusive)
+$randomNum = rand(1, 100);
+
+// Print the generated number to the console
+echo "Random Number: $randomNum\n";
